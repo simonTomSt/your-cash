f5729f1d0a24696452d83f5005379049a6cb519a
Szymon Stepien <st.szymon.tom2001@gmail.com>
2021-03-12 16:26:27 +0100

    Initial commit

---
+++ server/src/main.ts
@@ -0,0 +1,14 @@
+import config from 'config';
+
+import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const serverConfig = config.get('server');
+
+  const port = process.env.PORT || serverConfig.port;
+  await app.listen(port);
+}
+bootstrap();
